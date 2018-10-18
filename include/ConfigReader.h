fa747b0e2bff14c6e08b9f4a061b7b8ae725a3eb
Jakaria Blaine <jakariablaine120@gmail.com>
2018-10-19 00:19:05 +0600

    Add ConfigReader

---
+++ include/ConfigReader.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <Reader.h>
+#include <fstream>
+
+class ConfigReader : Reader
+{
+private:
+    std::fstream file;
+
+public:
+    ConfigReader(std::string file_path);
+    ~ConfigReader();
+
+    bool exists(std::string key);
+
+    std::string get(std::string key);
+
+    void add(std::string key, std::string value);
+
+    void set(std::string key, std::string val);
+
+    void remove(std::string key);
+};
---
+++ src/ConfigReader.cpp
@@ -0,0 +1,40 @@
+#include "ConfigReader.h"
+#include <fstream>
+#include <helpers.h>
+#include <vector>
+
+ConfigReader::ConfigReader(std::string file_path)
+{
+    file.open(file_path, std::fstream::in | std::fstream::out);
+}
+
+ConfigReader::~ConfigReader()
+{
+    file.close();
+}
+
+bool ConfigReader::exists(std::string key)
+{
+    std::string line;
+    while(std::getline(file, line))
+    {
+        std::vector<std::string> line_items = helpers::explode('=', line);
+        if(line_items[0] == key)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::string ConfigReader::get(std::string key)
+{ }
+
+void ConfigReader::add(std::string key, std::string value)
+{ }
+
+void ConfigReader::set(std::string key, std::string val)
+{ }
+
+void ConfigReader::remove(std::string key)
+{ }
