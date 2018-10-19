104ab8673dae1a76585e8c298b6c991098cb3f57
Jakaria Blaine <jakariablaine120@gmail.com>
2018-10-19 20:42:34 +0600

    Add ConfigReader initializing into the test cases and Add test for add()

--- test/TestConfigReader.cpp
+++ test/TestConfigReader.cpp
@@ -1,25 +1,44 @@
 #include <catch2/catch.hpp>
 #include "../src/ConfigReader.hpp"
-
-ConfigReader config("../auto_bkup_configs.txt");
+#include <iostream>
 
 TEST_CASE("file is loaded", "[config_reader]")
 {
+    ConfigReader config("../auto_bkup_configs.txt");
+
+    config.open();
+
     REQUIRE(config.loaded());
 }
 
 TEST_CASE("'path' key exists", "[config_reader]")
 {
+    ConfigReader config("../auto_bkup_configs.txt");
+
     REQUIRE(config.exists("path"));
 }
 
 TEST_CASE("'xyz' key doesn't exists", "[config_reader]")
 {
+    ConfigReader config("../auto_bkup_configs.txt");
+
     REQUIRE(!config.exists("xyz"));
 }
 
 TEST_CASE("'path' key have '~/destination' value", "[config_reader]")
 {
+    ConfigReader config("../auto_bkup_configs.txt");
+
     REQUIRE(config.get("path") == "~/destination");
 }
 
+TEST_CASE("successfully adds a new configuration to config file", "[config_reader]")
+{
+    ConfigReader config("../auto_bkup_configs.txt");
+
+    std::string key = "testing",
+                value = "it works";
+    config.add(key, value);
+
+    REQUIRE(config.get(key) == value);
+}
